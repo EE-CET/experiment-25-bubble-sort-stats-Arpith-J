8a07e5d947d0e513fe3fa844a48371baf63633ff
Arpith Joseph <arpithjoseph2005@gmail.com>
2026-02-22 20:01:30 +0530

    Code added

--- src/BubbleSortStats.java
+++ src/BubbleSortStats.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 public class BubbleSortStats {
-    
+        static Scanner sc = new Scanner(System.in);
         // TODO: Read n
         // TODO: Read the array elements
         // TODO: Implement Bubble Sort
@@ -7,5 +8,25 @@ public class BubbleSortStats {
         // TODO: Print total swaps
         // TODO: Print the first element of the sorted array
         // TODO: Print the last element of the sorted array
-    
+        public static void main(String[] args) {
+                int n = sc.nextInt();
+                int[] arr = new int[n];
+                for(int i=0; i<n; i++){
+                        arr[i] = sc.nextInt();
+                }
+                int swap = 0;
+                for(int i=0; i<n-1; i++){
+                        for(int j=0; j<n-i-1;j++){
+                                if(arr[j]>arr[j+1]){
+                                        int temp = arr[j];
+                                        arr[j] = arr[j+1];
+                                        arr[j+1] = temp;
+                                        swap++;
+                                }
+                        }
+                }
+                System.out.println(swap);
+                System.out.println(arr[0]);
+                System.out.println(arr[n-1]);
+        }
 }
